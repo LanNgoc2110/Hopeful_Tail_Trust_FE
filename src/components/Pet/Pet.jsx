06780bc1352cbe0ced0275c53a69b233492ab13b
LanNgoc2110 <ngocdl2110@gmail.com>
2024-11-23 18:44:04 +0700

    fix vercel 404 not found

--- src/components/Pet/Pet.jsx
+++ src/components/Pet/Pet.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react'
 import './Pet.css'
 
 import pet_image from '/assets/pitbull.png'
-import { Input, Pagination, Select } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Input, Pagination, Select, Spin } from 'antd';
 import not_found from "/assets/not-found.png"
 import { useLocation, useNavigate } from 'react-router-dom';
 import image_3 from '/assets/dogs.png'
-import { petApi} from '../../apis/pet.request';
+import { petApi } from '../../apis/pet.request';
 
 const { Search } = Input;
 
@@ -45,6 +46,7 @@ const Pet = () => {
     const [speciesValue, setSpeciesValue] = useState(null)
     const [genderValue, setGenderValue] = useState(null)
     const [currentPage, setCurrentPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
 
     const [pets, setPets] = useState([])
 
@@ -60,7 +62,7 @@ const Pet = () => {
     const filteredPets = /*listUnadoptedPets*/pets.filter(pet => {
         const searchTermLower = searchTerm.toLowerCase();
         return (
-            (!genderValue || pet.gender.toLowerCase() === genderValue) &&
+            (!genderValue || pet.sex === genderValue) &&
             (!speciesValue || pet.species === speciesValue) &&
             (!searchTerm || pet.name.toLowerCase().includes(searchTermLower))
             // dành cho nút search có thể search tất cả
@@ -97,10 +99,12 @@ const Pet = () => {
     }, [location]);
 
     useEffect(() => {
+        setIsLoading(true);
         const fetchDataPets = async () => {
             const response = await petApi.getAllPets()
             setPets(response.data.data || [])
-            console.log(response);
+            setIsLoading(false);
+            // console.log(response);
         }
         fetchDataPets()
     }, [])
@@ -149,8 +153,8 @@ const Pet = () => {
                             marginLeft: '3%',
                         }}
                         options={[
-                            { value: 'chó', label: 'Chó' },
-                            { value: 'mèo', label: 'Mèo' },
+                            { value: 'Dog', label: 'Chó' },
+                            { value: 'Cat', label: 'Mèo' },
                         ]}
                     />
 
@@ -184,7 +188,22 @@ const Pet = () => {
                     data-items={paginatedPets.length < 3 ? paginatedPets.length : 3}
                 >
                     <p className='title'>Danh sách thú cưng</p>
-                    {paginatedPets.length === 0 ? (
+                    {isLoading &&
+                        <div className="not-found">
+                            <Spin
+                                indicator={
+                                    <LoadingOutlined
+                                        style={{
+                                            margin: 150,
+                                            fontSize: 100,
+                                            color: 'var(--color-btn-auth)'
+                                        }}
+                                        spin />
+                                }
+                            />
+                        </div>
+                    }
+                    {!isLoading && paginatedPets.length === 0 ? (
                         <div className="not-found">
                             <img src={not_found} />
                             <p className="no-pet">The pet you are looking for is currently not available.</p>
@@ -196,7 +215,7 @@ const Pet = () => {
                                 <div className="overlay">
                                     <button
                                         className="view-more-button"
-                                        onClick={() => navigate(`/adoption/${item._id}`, { state: { pet: item, searchTerm, scrollY: window.scrollY, currentPage: currentPage  } }, window.scrollTo(0, 0))}
+                                        onClick={() => navigate(`/adoption/${item._id}`, { state: { pet: item, searchTerm, scrollY: window.scrollY, currentPage: currentPage } }, window.scrollTo(0, 0))}
                                     >
                                         Xem thêm
                                     </button>
@@ -204,7 +223,7 @@ const Pet = () => {
                                 <div className="pet-unadopted-item-info">
                                     <p>Tên: {item.name}</p>
                                     <p>Giới tính: {item.sex === 'Female' ? 'Cái' : 'Đực'}</p>
-                                    <p>Loài: {item.species}</p>
+                                    <p>Loài: {item.species === 'Dog' ? 'Chó' : 'Mèo'}</p>
                                     {/* <p>Tiêm ngừa: {item.vaccination}</p>
                                     <p>Tình trạng sức khỏe: {item.health_status}</p> */}
                                 </div>
